12a3ae0f011fe3ca2c9477c42cf828d00e0106cc
Wendell Suazo <wendellchansuazo11@gmail.com>
2020-12-09 16:43:08 +0800

    Block Scope with let and const

---
+++ Block Scope With let & const/app.js
@@ -0,0 +1,25 @@
+// Global Scope
+var a = 1;
+let b = 2;
+const c = 3;
+
+// function test(){
+//     var a = 4;
+//     let b = 5;
+//     const c = 6;
+//     
+// }
+// test();
+
+// if(true){
+//     // Block Scope
+//     var a = 4;
+//     let b = 5;
+//     const c = 6;
+//     console.log('IF SCOPE: ',a,b,c)
+// }
+
+for(let a = 0; a<10; a++){
+    console.log(`Loop: ${a}`);
+}
+console.log('GLOBAL SCOPE: ',a,b,c)

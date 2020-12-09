3c87797259eb2d6de02d44ab84071c5e24981511
Wendell Suazo <wendellchansuazo11@gmail.com>
2020-12-09 10:04:42 +0800

    first commit

---
+++ data_types.js
@@ -0,0 +1,27 @@
+// PRIMITIVE Data types
+//String
+const name = 'john doe';
+// number
+const age = 21;
+//boolean
+const isMale = true;
+//null
+const car = null;
+//undefined
+let test;
+//symbol
+const sym = Symbol()
+
+// REFERENCE TYPES - Objects
+//Array
+const hobbies = ['coding', 'sleeping', 'watching anime'];
+//object literal
+const address = {
+    'city' : 'calamba',
+    'province' : 'laguna',
+}
+
+const today = new Date();
+
+console.log(today);
+console.log(typeof today);

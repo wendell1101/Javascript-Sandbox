7ed9c056620e996e2b8c609ce198a2626d9ea092
Wendell Suazo <wendellchansuazo11@gmail.com>
2020-12-09 16:06:19 +0800

    General Loops

---
+++ General Loops/app.js
@@ -0,0 +1,70 @@
+// FOR LOOP
+
+// for(let i = 0; i<=10; i++){
+//     if(i === 2){
+//         console.log('My favorite number is ',i);
+//         continue;
+//     }
+//     if(i === 5){
+//         console.log('Stop the loop');
+//         break;
+//     }
+//     console.log('Number: ',i);
+// }
+
+// WHILE LOOP
+
+// let i = 0;
+// while(i <= 10){
+//     console.log(i);
+//     i++
+// }
+
+// DO WHILE
+
+// let i = 0;
+// do{
+//     console.log(i);
+//     i++
+// }while(i < 10);
+
+// LOOP THROUGH ARRAY
+// const cars = ['ford', 'toyota', 'honda'];
+
+// for(let i = 0; i < cars.length;i++){
+//     console.log(cars[i]);
+// }
+
+// FOREACH
+// cars.forEach(function(car, index, array){{
+//     console.log(`${index}: ${car}`);
+//     console.log(array);
+// }})
+
+// MAP
+
+// const users = [
+//    {id:1, name: 'John'},
+//    {id:2, name: 'Jane'},
+//    {id:3, name: 'Wendell'},
+//    {id:4, name: 'Julia'},
+// ];
+// const ids = users.map(function(user){
+//     return user.id;
+// });
+// console.log(ids);
+
+// users.forEach(function(user){
+//     console.log(user.name);
+// })
+
+// const user = {
+//     firstName : 'wendell',
+//     lastName : 'suazo',
+//     age: 21,
+// };
+
+// // FOR IN LOOP
+// for(let x in user){
+//     console.log(`${x} : ${user[x]}`);
+// }

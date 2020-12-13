12a4750a66e98132eced02053f404cce93e15579
Wendell Suazo <wendellchansuazo11@gmail.com>
2020-12-13 18:42:34 +0800

    Github Finder

---
+++ Github Finder/github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '4511ceb0ac1fd8de955c';
+    this.client_secret = '71eee0e7b0b9783ee72452e35e75b4229bf05cda';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
